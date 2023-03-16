e2978d39debb5a78a078faa21e41f32172d17ed1
Thalia Mariana <thaliamariana@live.com>
2023-03-15 21:41:24 -0300

    insere classsList para modificar a classe da pergunta selecionada

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.querySelectorAll(".arrow-down").forEach(changeStyleAfterClick);
 
 function changeStyleAfterClick(item, indice) {
-
+    
     var listaRespostas = document.querySelectorAll(".answer");
     var listaPerguntas = document.querySelectorAll(".question");
     
@@ -10,15 +10,13 @@ function changeStyleAfterClick(item, indice) {
         if (listaRespostas[indice].style.display == 'none')
         {
             listaRespostas[indice].style.display = 'block';
-            listaPerguntas[indice].style.color = '#1d1e35';
-            listaPerguntas[indice].style.fontWeight = 'bold';
+            listaPerguntas[indice].classList.add("question-selected");
             item.style.transform = 'rotate(0.5turn)';
         }
         else
         {
             listaRespostas[indice].style.display = 'none';
-            listaPerguntas[indice].style.fontWeight = 'normal';
-            listaPerguntas[indice].style.color = '#787887';
+            listaPerguntas[indice].classList.remove("question-selected");
             item.style.transform = 'rotate(0turn)';
         }   
     }
