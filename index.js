809d153b349d4ca11c837f3617c884775142f9e7
Thalia Mariana <thaliamariana@live.com>
2023-04-01 23:29:58 -0300

    cria funções para abertura e fechamento das perguntas
    permite apenas uma pergunta aberta por vez

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 document.querySelectorAll(".arrow-down").forEach(changeStyleAfterClick);
 
-function changeStyleAfterClick(item, indice) {
-    
-    var listaRespostas = document.querySelectorAll(".answer");
-    var listaPerguntas = document.querySelectorAll(".question");
-    
+var answerList = document.querySelectorAll(".answer");
+var questionList = document.querySelectorAll(".question");
+var lastIndex;
+
+function changeStyleAfterClick(item, index) {
+
     item.onclick = () => {   
 
-        if (listaRespostas[indice].style.display == 'none')
-        {
-            listaRespostas[indice].style.display = 'block';
-            listaPerguntas[indice].classList.add("question-selected");
-            item.style.transform = 'rotate(0.5turn)';
-        }
-        else
-        {
-            listaRespostas[indice].style.display = 'none';
-            listaPerguntas[indice].classList.remove("question-selected");
-            item.style.transform = 'rotate(0turn)';
+        if (answerList[index].style.display == 'none') {
+
+            if (lastIndex != undefined) {
+                closeQuestion(item, lastIndex);
+            }
+        
+            openQuestion(item, index);
+            lastIndex = index;
+
+        } else {
+            closeQuestion(item, index);
         }   
     }
+}
+
+function openQuestion(item, element)  {
+    answerList[element].style.display = 'block';
+    questionList[element].classList.add("question-selected");
+    item.style.transform = 'rotate(0.5turn)';
+}
+
+function closeQuestion(item, element) {
+    answerList[element].style.display = 'none';
+    questionList[element].classList.remove("question-selected");
+    item.style.transform = 'rotate(0turn)';
 }
